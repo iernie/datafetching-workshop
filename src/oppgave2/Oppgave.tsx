0b1ebf1afc416f3957cc2085a5410e3a0cb3eb99
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2024-04-16 23:30:50 +0200

    2c

--- src/oppgave2/Oppgave.tsx
+++ src/oppgave2/Oppgave.tsx
@@ -1,42 +1,38 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { errorQueryKey, getError } from "../api/errors";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const Oppgave2 = () => {
   /**
-   * OPPGAVE 2.a
-   * Endre fra en try-catch til å bruke den innebygde error-state fra valgt lib
-   * Se at den prøver å laste inn på nytt i dev tools
-   */
-  const {
-    data: todos,
-    error,
-    isLoading,
-  } = useQuery({
-    queryKey: [errorQueryKey],
-    queryFn: getError,
-  });
-
-  /**
-   * OPPGAVE 2.b
-   * Endre loading-logikken til å bruke isLoading prop fra valgt lib
+   * OPPGAVE 2.c
+   * Bonusoppgave: Kan du erstatte både error og isLoading med en suspense?
    */
   return (
     <>
       <h1>Todos</h1>
-      {isLoading && <>Loading...</>}
-      {error && <h2>Noe gikk galt ved henting av data</h2>}
-      <ul>
-        {todos?.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <ErrorBoundary fallback={<h2>Noe gikk galt ved henting av data</h2>}>
+        <Suspense fallback={<>Loading...</>}>
+          <TodoListe />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
 
-/**
- * OPPGAVE 2.c
- * Bonusoppgave: Kan du erstatte både error og isLoading med en suspense?
- */
+function TodoListe() {
+  const { data: todos } = useSuspenseQuery({
+    queryKey: [errorQueryKey],
+    queryFn: getError,
+  });
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.id}>{todo.title}</li>
+      ))}
+    </ul>
+  );
+}
 
 export default Oppgave2;
