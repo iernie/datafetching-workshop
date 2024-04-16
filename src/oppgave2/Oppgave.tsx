c4a9ce32b4b613f42f6290c14ed9bc217c283705
Joakim Gyllenskepp <joakim.gyllenskepp@vegvesen.no>
2024-04-16 23:26:10 +0200

    2a

--- src/oppgave2/Oppgave.tsx
+++ src/oppgave2/Oppgave.tsx
@@ -1,28 +1,16 @@
-import { useEffect, useState } from "react";
-import { Todo } from "../types";
-import { getError } from "../api/errors";
+import { useQuery } from "@tanstack/react-query";
+import { errorQueryKey, getError } from "../api/errors";
 
 const Oppgave2 = () => {
-  const [error, setError] = useState<boolean>(false);
-  const [todos, setTodos] = useState<Todo[]>([]);
-
   /**
    * OPPGAVE 2.a
    * Endre fra en try-catch til å bruke den innebygde error-state fra valgt lib
    * Se at den prøver å laste inn på nytt i dev tools
    */
-  useEffect(() => {
-    const getItems = async () => {
-      try {
-        const result = await getError();
-        setTodos(result);
-        setError(false);
-      } catch {
-        setError(true);
-      }
-    };
-    getItems();
-  }, []);
+  const { data: todos, error } = useQuery({
+    queryKey: [errorQueryKey],
+    queryFn: getError,
+  });
 
   /**
    * OPPGAVE 2.b
@@ -31,10 +19,10 @@ const Oppgave2 = () => {
   return (
     <>
       <h1>Todos</h1>
-      {todos.length === 0 && !error && <>Loading...</>}
+      {todos?.length === 0 && !error && <>Loading...</>}
       {error && <h2>Noe gikk galt ved henting av data</h2>}
       <ul>
-        {todos.map((todo) => (
+        {todos?.map((todo) => (
           <li key={todo.id}>{todo.title}</li>
         ))}
       </ul>
